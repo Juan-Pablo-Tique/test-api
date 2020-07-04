456f9a69d8beba3b48f993dafba888316fc783a4
Juan Pablo Tique Sogamoso <jtique@econtainers.co>
2020-07-04 07:43:37 -0500

    version 1.00

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ReportComponent } from './components/report/report.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
@@ -21,7 +26,13 @@ import { NgxPaginationModule } from 'ngx-pagination';
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
     NgxPaginationModule,
   ],
   providers: [],
--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-report',
@@ -9,13 +11,43 @@ import { ApiService } from '../../services/api.service';
 export class ReportComponent implements OnInit {
   public dataAll: any;
   public viewPage: number = 1;
+  public amountDataDisplayed: number = 5;
+  public optionSelect: Array<number> = [5, 10, 15, 20];
+  public selected = 5;
+  public textSearch: string = '';
+  public numberRecords: number = 0;
+  public reportForm: FormGroup = this.formBuilder.group({
+    search: [null, [Validators.required]],
+    rows: [null, [Validators.required]],
+  });
 
-  constructor(private request: ApiService) {}
+  constructor(private request: ApiService, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.reportForm
+      .get('search')
+      ?.valueChanges.pipe(debounceTime(600))
+      .subscribe((value) => {
+        this.getSearch(value);
+      });
+
+    this.reportForm.get('rows')?.valueChanges.subscribe((res) => {
+      this.amountDataDisplayed = res;
+    });
+
+    this.getSearch();
+  }
+
+  getSearch(value?: string) {
     this.request.getDataTable().subscribe(
       (res) => {
         this.dataAll = res;
+        if (value) {
+          this.dataAll = this.dataAll.filter((e: any) =>
+            e.title.includes(value)
+          );
+          this.textSearch = value;
+        }
       },
       (err) => {
         console.log(err);
